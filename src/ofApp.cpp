2773bcdbda868efe259d44f3c115f3922a2edde8
Emma Souannhaphanh <77692774+etretf@users.noreply.github.com>
2023-12-02 12:34:15 -0500

    Revert "com"
    This reverts commit c1339fd27a8e95098259b92ad0f4262f6b59d084.

--- src/ofApp.cpp
+++ src/ofApp.cpp
@@ -2,23 +2,7 @@
 
 //--------------------------------------------------------------
 void ofApp::setup(){
-	ofSetWindowShape(Config::APP_WINDOW_WIDTH, Config::APP_WINDOW_HEIGHT);
-	ofSetFrameRate(Config::APP_DESIRED_FRAMERATE);
 
-
-	//init vars
-	m_spaceshipPos.set(Config::APP_WINDOW_WIDTH / 2.0f, Config::APP_WINDOW_HEIGHT - 100.0f);
-	m_spaceshipRadius = 50.0f;
-	m_spaceshipAngle = 0.0f;
-	m_leftJetForce = 0.0f;
-	m_rightJetForce = 0.0f;
-
-	//load images
-	m_startMenu.load("spaceStartMenu.png");
-	m_gameMode = start; //0
-	
-	//ImGUI setup
-	m_gui.setup();
 }
 
 //--------------------------------------------------------------
@@ -28,34 +12,17 @@ void ofApp::update(){
 
 //--------------------------------------------------------------
 void ofApp::draw(){
-	ofSetColor(255);
-	m_startMenu.draw(0, 0);
-	ofSetColor(ofColor::red);
-	ofDrawCircle(m_spaceshipPos, m_spaceshipRadius - 5.0f);
+
 }
 
 //--------------------------------------------------------------
 void ofApp::keyPressed(int key){
-	if (key == 'A' || key == 'a')
-	{
-		m_leftJetForce += 0.5f;
-	}
-	if (key == 'D' || key == 'd')
-	{
-		m_leftJetForce += 0.5f;
-	}
+
 }
 
 //--------------------------------------------------------------
 void ofApp::keyReleased(int key){
-	if (key == 'A' || key == 'a')
-	{
-		m_leftJetForce = 0.0f;
-	}
-	if (key == 'D' || key == 'd')
-	{
-		m_rightJetForce = 0.0f;
-	}
+
 }
 
 //--------------------------------------------------------------
